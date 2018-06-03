2c8c781a9e0e0874f08b59bc89983f213805a5ea
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-06-02 21:01:04 -0300

    Criando a classe de contexto e a Classe ator. Utilizando anotações para poder explicitar a quebra de convenção do nome da tabela e colunas com a classe

---
+++ Alura.Filmes.App/Dados/FilmeContexto.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class FilmeContexto: DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database =FILME; Trusted_Connection = true;");
+        }
+    }
+}
--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using System;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var context = new FilmeContexto())
+            {
+                foreach (var atores in context.Atores)
+                {
+                    System.Console.WriteLine(atores.ToString());
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
