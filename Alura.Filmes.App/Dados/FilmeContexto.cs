35823f136cd955f1c6c277777faa90a7cda75ea9
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-06-14 14:54:53 -0300

    Utilizando Fluent API no lugar de Data Anotations

--- Alura.Filmes.App/Dados/FilmeContexto.cs
+++ Alura.Filmes.App/Dados/FilmeContexto.cs
@@ -15,5 +15,28 @@ namespace Alura.Filmes.App.Dados
         {
             optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database =FILME; Trusted_Connection = true;");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ator>()
+                .ToTable("actor");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+
+        }
     }
 }
