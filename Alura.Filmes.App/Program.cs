fc4a541c8fd6d8520ccee9451f3d7eb0299e91ec
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-06-17 01:51:58 -0300

    Utilizando procedures no EF Core

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -4,6 +4,7 @@ using Alura.Filmes.App.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Data.SqlClient;
 
 namespace Alura.Filmes.App
 {
@@ -18,12 +19,29 @@ namespace Alura.Filmes.App
                 {
                     try
                     {
-                        var topAtores = context.VWAtorFilmes.ToList();
+                        //var topAtores = context.VWAtorFilmes.ToList();
 
-                        foreach (var ator in topAtores)
-                        {
-                            Console.WriteLine(ator);
-                        }
+                        //foreach (var ator in topAtores)
+                        //{
+                        //    Console.WriteLine(ator);
+                        //}
+
+                        //var categ = "Action"; //36
+
+                        //var paramCateg = new SqlParameter("category_name", categ);
+
+                        //var paramTotal = new SqlParameter
+                        //{
+                        //    ParameterName = "@total_actors",
+                        //    Size = 4,
+                        //    Direction = System.Data.ParameterDirection.Output
+                        //};
+
+                        //        context.Database
+                        //            .ExecuteSqlCommand("total_actors_from_given_category @category_name, @total_actors OUT", paramCateg, paramTotal);
+
+                        //System.Console.WriteLine($"O total de atores na categoria {categ} é de {paramTotal.Value}.");
+
 
                     }
                     catch (Exception ex)
